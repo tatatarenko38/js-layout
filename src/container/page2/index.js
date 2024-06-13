9d4860b05c76cc17db8045a18addd6f906990fd6
Татьяна Татаренко <tatatatarenko38@gmail.com>
2024-06-13 17:38:47 +0300

    lay

--- src/container/page2/index.js
+++ src/container/page2/index.js
@@ -52,34 +52,87 @@ page2.append(title2)
 // const info = createInfo()
 // page2.append(info)
 
+const TAB_LIST = [
+  {
+    info: 'База знань',
+    active: true,
+  },
+  {
+    info: 'Інформація',
+    active: false,
+  },
+]
+
+const createChange = () => {
+  const changeTwo = createElement('div', 'change')
+
+  TAB_LIST.forEach((tab) => {
+    const change = createElement(
+      'div',
+      tab.active
+        ? 'change__ch change--viewed'
+        : 'change__ch',
+    )
+
+    const text = createElement(
+      'div',
+      'change__text',
+      tab.info,
+    )
+
+    const under = createElement('div', 'change__under')
+
+    change.append(text, under)
+
+    changeTwo.append(change)
+  })
+
+  return changeTwo
+}
+
+const change = createChange()
+
+page2.append(change)
+
 const INFO_LIST = {
-  text1: 'База знань',
-  text2: 'Інформація',
   src: '/img/image__1.png',
   title: 'Що таке база знань?',
-  info: `База знаний — база данных, содержащая правила вывода и информацию о человеческом опыте и
+  text: `База знаний — база данных, содержащая правила вывода и информацию о человеческом опыте и
   знаниях в некоторой предметной области. В самообучающихся системах база знаний также содержит информацию,
   являющуюся результатом решения предыдущих задач.`,
   button: `Перейти до ком'юніті у Телеграм`,
 }
 
 const createInfo = () => {
-  const infoList = createElement('main', 'info__list')
+  const infoBox = createElement('div', 'info')
+  const img = createElement('img', 'info__img')
 
-  const tabs = createElement('div', 'tabs')
-  const tab1 = createElement(
-    'button',
-    'tabs__button',
-    info.text1,
+  img.src = INFO_LIST.src
+
+  infoBox.appendChild(img)
+
+  const title = createElement(
+    'div',
+    'info__title',
+    INFO_LIST.title,
   )
-  const tab2 = createElement(
-    'button',
-    'tabs__button',
-    info.text2,
+
+  infoBox.appendChild(title)
+
+  const textContainer = createElement(
+    'div',
+    'info__text',
+    INFO_LIST.text,
   )
-  tabs.append(tab1, tab2)
-  info.append(tabs)
+
+  infoBox.appendChild(textContainer)
+
+  const button = createElement('button', 'info__button', INFO_LIST.button)
+
+  infoBox.appendChild(button)
+
+  return infoBox
 }
+const info =  createInfo()
 
-const info = createInfo()
 page2.append(info)
